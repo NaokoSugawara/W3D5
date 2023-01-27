c600ecb8c5985403448f4adf659ebd57888a6fe6
Naoko Sugawara <infoseek122@gmail.com>
2023-01-27 15:39:44 -0500

    add parent=

--- skeleton/skeleton/lib/00_tree_node.rb
+++ skeleton/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,19 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(new_parent)
+        @parent.children.delete(self) if !@parent.nil?
+        @parent = new_parent
+        if !new_parent.nil?
+            new_parent.children << self if !new_parent.children.include?(self)
+        end
+    end
+
 end
